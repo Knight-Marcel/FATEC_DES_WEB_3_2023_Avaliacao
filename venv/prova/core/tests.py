6c2141e6c6f71b1b13f8e8dee32f3c3929a36ce5
Marcel de Menezes Araujo <araujomarcel89@gmail.com>
2023-06-14 17:45:34 -0300

    final

--- venv/prova/core/tests.py
+++ venv/prova/core/tests.py
@@ -8,3 +8,22 @@ class Presenca(TestCase):
         self.assertContains(self.resp, 'index')
     def test_template_natal(self):
         self.assertTemplateUsed(self.resp, 'cadastro.html')    
+    def test_cadastro_sucesso(self):
+        # Preparação dos dados de teste
+        data = {
+            'nomealuno': 'João',
+            'nomeprofessor': 'Maria',
+        }
+        
+        # Simulando uma requisição POST para a URL de cadastro
+        response = self.client.post('/cadastro/', data)
+        
+        # Verificação do resultado esperado
+        self.assertEqual(response.status_code, 200)
+        
+        # Verificação de que o objeto foi salvo no banco de dados
+        self.assertEqual(Presenca.objects.count(), 1)
+        presenca = Presenca.objects.first()
+        self.assertEqual(presenca.nomealuno, 'João')
+        self.assertEqual(presenca.nomeprofessor, 'Maria')
+        
